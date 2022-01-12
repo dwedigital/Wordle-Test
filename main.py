1ed411b56b6d1b9d1c5a4ae1e73ba1700b9011da
dwedigital <dave@dwedigital.com>
2022-01-12 08:10:20 +0000

    Testing new list

--- main.py
+++ main.py
@@ -2,72 +2,59 @@ import csv
 import re
 
 words = set()
-
-incorrectLetters = []
-knownLetters = []
-
-
-def wordList():
-    with open('words.csv', 'r') as csvfile:
-        reader = csv.reader(csvfile)
-        for row in reader:
-            words.add(row[0])
+# Only add letters that are known and '.' for unknown or incorrect (placement)
+guess = ".rin."
+knownLetters = ['d']
+incorrectLetters = ['a','e','u','g']
+# Pattern is not currently in use but was for regex
+pattern = "|".join(incorrectLetters)
+possibles = []
 
 
-def guessWord(guess):
-    possibles = []
-    revisedPossibles = []
-    for i in words:
-        if re.match(rf"{guess}", i):
-            possibles.append(i)
-
-    # Out of those words which have all the known letters
-    for index, word in enumerate(possibles):
-        test = 0
-        for letter in knownLetters:
-            if letter in word:
-                test += 1
-        if test != len(knownLetters):
-            possibles.remove(word)
-
-    # Out of the possibles which words contain letters known not to be correct
-
-    for index, word in enumerate(possibles):
-        test = 0
-        for letter in incorrectLetters:
-            if letter in word:
-                test += 1
-
-        if test == 0:
-            revisedPossibles.append(word)
-
-    revisedPossibles.sort()
-    print("Possibles:\n")
-    for i in revisedPossibles:
-        print(i)
+with open('words.csv', 'r') as csvfile:
+    reader = csv.reader(csvfile)
+    for row in reader:
+        words.add(row[0])
 
 
-if __name__ == "__main__":
-    wordList()
+# This loop just checks for possibilites matching the guess pattern. Only useful if some known letters are found
+for i in words:
+  #TODO - check f strings are not cocking this up but appears to work as expected with and without f string
 
-    while True:
 
-        print(
-            "\nPlease enter your guess with known correct letters and full stops for incorrect letters or letters in the wrong place"
-        )
-        inputGuess = input("> ")
+  if re.match(rf"{guess}",i):
+    possibles.append(i)
 
-        print("\nPlease enter letters known not to be in the word")
-        inputWrongLetters = input().lower()
-        if len(inputWrongLetters) > 0:
-            for i in inputWrongLetters:
-                incorrectLetters.append(i.lower())
+newpossibles=[]
 
-        print("\nEnter letters known to be in the word but wrong location")
-        inputLettersInWord = input()
-        if len(inputLettersInWord) > 0:
+print("Known unknown letters: ", pattern)
+# Out of those words which have all the known letters
+for index, word in enumerate(possibles):
+  test = 0
+  for letter in knownLetters:
+    if letter in word:
+      test +=1
+  if test == len(knownLetters):
+    newpossibles.append(word)
 
-            for i in inputLettersInWord:
-                knownLetters.append(i.lower())
+# Out of the possibles which words contain letters known not to be correct
 
-        guessWord(inputGuess)
+revisedPossibles = []
+for index, word in enumerate(newpossibles):
+  # Test is used as a test mechanism to try and debug the weirdness 
+  test=0
+  for letter in incorrectLetters:
+    
+    # print(letter)
+    if letter in word:
+      test+=1 #NOPE! Multi letters. You need to keep the flag. Or use any :) smart!
+
+      
+  if test == 0:
+    revisedPossibles.append(word)
+
+
+revisedPossibles.sort()
+print("Possibles:\n")
+for i in revisedPossibles:
+  print(i)
